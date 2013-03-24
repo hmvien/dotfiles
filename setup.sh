afc74e645aee867329b46a962caa9c39df76791e
Hien Vien <hienmvien@gmail.com>
2013-03-24 01:03:19 -0700

    added auto jump to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 # Only need to run this once 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+git clone git://github.com/joelthelion/autojump.git ~/autojump
+cd ~/autojump
+./install.sh
+cd ~
+
