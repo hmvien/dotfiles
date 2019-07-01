646cf1cd6670c5759bdc9805974f15e1f6d0be43
Hien <hienmvien@gmail.com>
2019-06-30 17:41:07 -0700

    Added bluetooth and compton

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ elif [[ "$(uname)" = Darwin* ]]; then
   ./setup_mac.sh
 fi
 
-stow home vim zsh git autorandr i3 i3blocks
+stow home vim zsh git autorandr i3 i3blocks compton
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
