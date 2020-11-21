65778862886e760974cec1cc5edb1bdbda1b23e9
Hien Vien <hvien@blend.com>
2020-11-20 19:59:48 -0800

    Update setup

--- setup.sh
+++ setup.sh
@@ -5,20 +5,17 @@ git submodule update --init --recursive
 if [[ "$(uname)" = Linux* ]]; then
   ./setup_linux.sh
 elif [[ "$(uname)" = Darwin* ]]; then
+  brew install zsh
   ./setup_mac.sh
 fi
 
 stow home nvim zsh git autorandr 
-#stow i3 i3blocks compton
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-#echo "Installing vim vundle..."
-#mkdir -p ~/.vim/bundle
-#git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/gcuisinier/jenv.git ~/.jenv
 
 if [[ "$(uname)" = Linux* ]]; then
@@ -39,14 +36,8 @@ mkdir -p ${local_setup_dir}
 cd ${local_setup_dir}
 
 
-echo "Installing autojump..."
-git clone git://github.com/joelthelion/autojump.git ${local_setup_dir}/autojump
-cd ${local_setup_dir}/autojump
-python ./install.py
-
-#echo "Installing i3blocks-contrib..."
-#cd ${local_setup_dir}
-#git clone git@github.com:vivien/i3blocks-contrib.git ${local_setup_dir}/i3blocks-contrib
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh
+./dein_installer.sh ~/.cache/dein
 
 cd ~/dotfiles
 
--- setup_mac.sh
+++ setup_mac.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+brew install macvim
 brew install python3
 brew install stow
+brew install scmpuff
 
