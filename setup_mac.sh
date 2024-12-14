a1fd033beca78e8004c2e5a95ab5e180f1015448
Hien Vien <hienmvien@gmail.com>
2024-12-14 00:36:35 -0800

    organize setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env zsh
 
-brew install neovim
-brew install vimr
-brew install python3
+# env
 brew install stow
+brew install direnv
+brew install jenv
+brew install mise
+
+# desktop
+brew install betterdisplay
+
+# terminal
+brew install tmux
+brew install --cask wezterm
+
+# cli tools
 brew install scmpuff
 brew install coreutils
 brew install findutils
 brew install tree
 brew install colordiff
-brew install direnv
-brew install jenv
-brew install mise
-brew install lazygit
-brew install betterdisplay
-brew install tmux
 brew install ripgrep
-brew install git-delta
-brew install fd
-brew install luarocks
-brew install shfmt
+brew install zoxide # fastjump
+brew install fd     # alternative to find
 
+# dev tools
+brew install python3
+brew install lazygit
+brew install git-delta
 brew install --cask temurin
+
+# neovim
+brew install neovim
+brew install vimr
+brew install shfmt
+brew install luarocks
 
 brew install font-fira-code
 brew install font-monaspace
